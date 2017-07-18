05b14f5276dfe74e7c971d741ddc6e059956d51b
jeanmichel <jeanmichel@sequans.com>
2017-07-18 19:06:06 +0200

    - add lucy cheat sheet (WIP)

---
+++ js/data_rogue.js
@@ -0,0 +1,37 @@
+data_rogue = [
+    {
+        title: "Sneak Attack",
+        icon: "high-shot",
+        subtitle: "1st level",
+        description: "Add 1d6 (or more) to your attacks when having an advantage",
+        reference: "PHB",
+        bullets: [
+			"Beginning at 1st level, you know how to strike subtly and exploit a foe's distraction. Once per turn, you can deal an <b>extra 1d6 damage</b> to one creature you hit with an Attack if you have advantage on the Attack roll. The Attack must use a Finesse or a ranged weapon.",
+			"You don't need advantage on the Attack roll if another enemy of the target is within 5 feet of it, that enemy isn't Incapacitated, and you don't have disadvantage on the Attack roll.",
+			"The amount of the extra damage increases as you gain levels in this class, as shown in the Sneak Attack column of the Rogue table."
+        ]
+    },
+    {
+        title: "Thieves Cant",
+        icon: "high-shot",
+        subtitle: "1st level",
+        description: "Hide message in conversation for other rogues",
+        reference: "PHB",
+        bullets: [
+			"During your rogue Training you learned thieves' cant, a secret mix of dialect, jargon, and code that allows you to hide messages in seemingly normal conversation. Only another creature that knows thieves' cant understands such messages. It takes four times longer to convey such a Message than it does to speak the same idea plainly.",
+		"In addition, you understand a set of secret signs and symbols used to convey short, simple messages, such as whether an area is dangerous or the territory of a thieves' guild, whether loot is nearby, or whether the people in an area are easy marks or will provide a safe house for thieves on the run"
+        ]
+    },
+	{
+        title: "Cunning action",
+        icon: "high-shot",
+        subtitle: "2nd level",
+        description: "Get a bonus action each turn to dash, disengage, or hide",
+        reference: "PHB",
+        bullets: [
+			"Beginning at 1st level, you know how to strike subtly and exploit a foe's distraction. Once per turn, you can deal an <b>extra 1d6 damage</b> to one creature you hit with an Attack if you have advantage on the Attack roll. The Attack must use a Finesse or a ranged weapon.",
+			"You don't need advantage on the Attack roll if another enemy of the target is within 5 feet of it, that enemy isn't Incapacitated, and you don't have disadvantage on the Attack roll.",
+			"The amount of the extra damage increases as you gain levels in this class, as shown in the Sneak Attack column of the Rogue table."
+        ]
+    }
+]
